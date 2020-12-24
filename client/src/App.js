6ae904e8c7ec88e71d3811c14616abfce5297f13
Chike <hyginuschinwoke@gmail.com>
2020-12-24 08:34:59 +0100

    setup apolloclient, add routering

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
+import './App.css';
+import MenuBar from './components/MenuBar';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Container>
+        <Router>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Router>
+      </Container>
+    </ApolloProvider>
+  );
+}
+
+export default App;
